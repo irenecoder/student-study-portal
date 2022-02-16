789378c40d2c885ac1a0974f661dea722c0c5117
irenecoder <wairene60@gmail.com>
2022-02-16 19:23:31 +0300

    create-homework-form

--- portal/views.py
+++ portal/views.py
@@ -30,4 +30,14 @@ class NotesDetailView(generic.DetailView):
     model = Notes
 
 def homework(request):
-    return render(request,'portal/homework.html')
+    form = HomeworkForm()
+    homework = Homework.objects.filter(user=request.user)
+    if len(homework)==0:
+        homework_done=True
+    else:
+        homework_done = False
+    context = {'homeworks':homework,'homeworks_done':homework_done,'form':form}
+    return render(request,'portal/homework.html',context)
+
+
+
