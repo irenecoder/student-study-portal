96a74afd5f6a92e7c4d4377906e4d9aa639913bd
irenecoder <wairene60@gmail.com>
2022-02-17 17:29:08 +0300

    check-box for finished homework

--- portal/views.py
+++ portal/views.py
@@ -30,7 +30,30 @@ class NotesDetailView(generic.DetailView):
     model = Notes
 
 def homework(request):
-    form = HomeworkForm()
+    if request.method == 'POST':
+        form = HomeworkForm(request.POST)
+        if form.is_valid():
+            try:
+                finished = request.POST['is_finished']
+                if finished == 'on':
+                    finished = True
+                else :
+                    finished = False
+            except :
+                finished = False
+            homeworks = Homework(
+                user = request.user,
+                subject = request.POST['subject'],
+                title = request.POST['title'],
+                description = request.POST['description'],
+                due = request.POST['due'],
+                is_finished = finished
+
+            )
+            homeworks.save()
+            messages.success(request,f'Homework added from {request.user.username}!!')
+    else:
+        form = HomeworkForm()
     homework = Homework.objects.filter(user=request.user)
     if len(homework)==0:
         homework_done=True
